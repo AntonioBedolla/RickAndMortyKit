bf7475e0c7562ead2a7dabf7088fbe366feb3bbc
José Antonio Campos <fred-mercury@hotmail.com>
2025-10-21 08:57:13 -0600

    Primer commit

---
+++ RickAndMortyKit/RickAndMortyKit/RickAndMortyKit.h
@@ -0,0 +1,18 @@
+//
+//  RickAndMortyKit.h
+//  RickAndMortyKit
+//
+//  Created by Antonio Bedolla on 21/10/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RickAndMortyKit.
+FOUNDATION_EXPORT double RickAndMortyKitVersionNumber;
+
+//! Project version string for RickAndMortyKit.
+FOUNDATION_EXPORT const unsigned char RickAndMortyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RickAndMortyKit/PublicHeader.h>
+
+
